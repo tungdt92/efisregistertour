4c032c3aaa3b06f9722e77b9501e40e97bc0e879
tungdt92 <0.bigdream.10@gmail.com>
2019-01-15 19:40:07 +0700

    add function check sheet already exist

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('APPID', '358481194710655');
+define('APPSECRET', 'a0628911b72c2e70fd908ab3cf68da78');
+define('EFISGROUPID', '611608949293895');
+define('FBCALLBACK', 'http://localhost/Efis/fb-callback.php');
+define('ERROR_NOTMEMBER', 'Có thể bạn không phải là thành viên CLB, vui lòng kiểm tra lại tài khoản facebook');
+define('ERROR_SYSTEM', 'Xảy ra lỗi hệ thống, vui lòng liên hệ Thủy để report');
+define('GOOGLESHEETID', '1jSY-Qfq18ynu_IYUgGSDNJe4NbgmfRcOBU0vb_3VxWA'); //https://docs.google.com/spreadsheets/d/1jSY-Qfq18ynu_IYUgGSDNJe4NbgmfRcOBU0vb_3VxWA/edit?usp=sharing
+
+
+
--- quickstart.php
+++ quickstart.php
@@ -1,18 +1,40 @@
 <?php
-
+require './constants.php';
 require __DIR__ . '/vendor/autoload.php';
 
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
 }
 
-function addNewSheet($service, $spreadsheetId) {
+function myArrayContainsWord(array $myArray, $word) {
+    foreach ($myArray as $element) {
+        if ($element->title == $word) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkSheetExist($service, $spreadsheetId, $sheetname) {
+    $sheetInfo = $service->spreadsheets->get($spreadsheetId);
+    $allsheet_info = $sheetInfo['sheets'];
+    $idCats = array_column($allsheet_info, 'properties');
+
+    if (myArrayContainsWord($idCats, $sheetname)) {
+        printf("tdtnewsheet sheet found\n");
+        return true;
+    }
+    return false;
+}
+
+function addNewSheet($service, $spreadsheetId, $sheetname) {
+
     $requests = [
         // Change the spreadsheet's title.
         new Google_Service_Sheets_Request([
             'addSheet' => [
                 'properties' => [
-                    'title' => 'tdtnewsheet'
+                    'title' => $sheetname
                 ]
             ]
                 ])
@@ -24,13 +46,10 @@ function addNewSheet($service, $spreadsheetId) {
     ]);
 
     $response = $service->spreadsheets->batchUpdate($spreadsheetId, $batchUpdateRequest);
-//    $findReplaceResponse = $response->getReplies()[1]->getFindReplace();
-    printf("%s replacements made.\n",
-            $response->properties->sheetId);
 }
 
 function getServiceAcountClient() {
-    $KEY_FILE_LOCATION =__DIR__ .'/efis register tour-3ad269a7bda0.json';
+    $KEY_FILE_LOCATION = __DIR__ . '/efis register tour-3ad269a7bda0.json';
     // Create and configure a new client object.
     $client = new Google_Client();
     $client->setApplicationName("Efis Register Tour");
@@ -42,27 +61,11 @@ function getServiceAcountClient() {
 // Get the API client and construct the service object.
 $client = getServiceAcountClient();
 $service = new Google_Service_Sheets($client);
+$spreadsheetId = GOOGLESHEETID;
 
-// Prints the names and majors of students in a sample spreadsheet:
-// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-//$spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms';
-//
-//https://docs.google.com/spreadsheets/d/1jSY-Qfq18ynu_IYUgGSDNJe4NbgmfRcOBU0vb_3VxWA/edit?usp=sharing
-$spreadsheetId = '1jSY-Qfq18ynu_IYUgGSDNJe4NbgmfRcOBU0vb_3VxWA';
+
+
+
 
 addNewSheet($service, $spreadsheetId);
 
-//$range = 'Class Data!A2:E';
-//
-//$response = $service->spreadsheets_values->get($spreadsheetId, $range);
-//$values = $response->getValues();
-//
-//if (empty($values)) {
-//    print "No data found.\n";
-//} else {
-//    print "Name, Major:\n";
-//    foreach ($values as $row) {
-//        // Print columns A and E, which correspond to indices 0 and 4.
-//        printf("%s, %s\n", $row[0], $row[4]);
-//    }
-//}
